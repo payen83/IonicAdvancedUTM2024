6917545c67fd785e25df3e727b204a2649ae9c97
Ferdy Fauzi <payen83@gmail.com>
2024-02-19 10:54:07 +0800

    upload image

--- src/app/pages/upload/upload.page.ts
+++ src/app/pages/upload/upload.page.ts
@@ -8,18 +8,43 @@ import { Component, OnInit } from '@angular/core';
 export class UploadPage implements OnInit {
   file_ext: string = '';
   file_view:  string = '';
-  
+  filename: string = '';
+  file!: File;
+
   constructor() { }
 
   ngOnInit() {
   }
 
   onFileChanged(fileChangeEvent: any){
+    this.file = fileChangeEvent.target.files[0];
+    this.filename = this.file.name;
+    const type = this.file.type;
 
+    //get file extension
+    const ext = this.filename.slice((this.filename.lastIndexOf(".") -1 >>> 0)+2);
+    console.log("file extension: ", ext);
+    const position = type.search('image');
+
+    //check if upload file is image
+    if(position >= 0){
+      const reader = new FileReader();
+      this.file_ext = '';
+      reader.readAsDataURL(this.file);
+      reader.onload = (event: any) => {
+        this.file_view = event.target.result;
+        // console.log(this.file_view);
+      };
+      reader.onerror = (error: any) => {
+        console.log('Error occured: ', error);
+      };
+    } else {
+      this.file_ext = ext;
+    }
   }
 
   uploadSingleFile(){
-    
+
   }
 
 }
