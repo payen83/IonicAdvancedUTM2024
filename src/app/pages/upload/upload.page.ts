db04cfef7d85433aa3a8626c2f09b9428b499b95
Ferdy Fauzi <payen83@gmail.com>
2024-02-20 12:33:42 +0800

    update upload

--- src/app/pages/upload/upload.page.ts
+++ src/app/pages/upload/upload.page.ts
@@ -14,14 +14,74 @@ export class UploadPage implements OnInit {
   file!: File;
   files_ext: string[] = [];
   fileCount: number = 0;
-  fileList: File[] = [];
+  fileList: any = [];
   filesname: string[] = [];
   constructor(public api: ApiService) { }
 
   ngOnInit() {
   }
 
-  onFilesChanged(filesChangeEvent: any){
+  deleteItem(i: number){
+    return this.fileList.splice(i, 1);
+  }
+
+  async onFilesChanged(filesChangeEvent: any){
+    for(let file_ of filesChangeEvent.target.files){
+      console.log(file_);
+      let ext_ = file_.name.slice((file_.name.lastIndexOf('.') -1 >>> 0) + 2);
+      let type_ = file_.type;
+      let view_: any = '';
+      let size: number = file_.size;
+      const allowedSize = 1024000;
+      if(file_.size>allowedSize){
+        alert('image ' + file_.name +' is more than 1MB');
+        continue;
+      }
+      console.log(ext_);
+      const positions = type_.search("image");
+      // if(ext_ != 'png' && ext_ != 'jpg'){
+      //   alert('image format for '+ file_.name + ' is not allowed');
+      //   continue;
+      // }
+      if(positions >=0){
+        //Option 1
+        try {
+          view_ = await this.getImageData(file_);
+          this.fileList.push({file: file_,filename: file_.name,type: type_,file_ext: ext_,file_view: view_});
+        } catch(error) {
+          console.log(error);
+        }
+        // Option 2
+        // this.getImageData(file_).then((result: any)=>{
+        //   view_ = result;
+        //   console.log('view retreived');
+        //   this.fileList.push({file: file_,filename: file_.name,type: type_,file_ext: ext_,file_view: view_});
+        // }).catch((err: any)=>{
+        //   console.log(err);
+        // })
+      } else {
+        alert('image format for '+ file_.name + ' is not allowed');
+    }  
+    } // end of for loop
+    console.log(this.fileList);
+  }
+
+  getImageData(file_: any){
+    return new Promise((resolve, reject)=>{
+      const reader = new FileReader();
+      reader.readAsDataURL(file_);
+      reader.onload = (event: any) => {
+        let view_ = event.target.result;
+        resolve(view_);
+      };
+      reader.onerror = (error) => {
+        console.log('Error', error);
+        reject(error);
+      };
+    });
+  }
+
+  onFilesChanged_old(filesChangeEvent: any){
     this.filesname = [];
     this.files_ext = [];
     this.fileList = [];
